a834154b017666f50ab9005bcc310ea85a6f5a55
anatoliii123 <64612850+anatoliii123@users.noreply.github.com>
2022-06-12 16:42:55 -0500

    encryption studying

---
+++ xor.c
@@ -0,0 +1,37 @@
+/*header files */
+#include <stdio.h>
+
+
+/* INPUT a STRING and save it in a VARIABLE */
+int main() {                                                                                                                                                                            
+  char stringinput[]; 
+  printf("Enter a input: ");                                                                                                                                                                
+  scanf("%s", stringinput);   
+  fflush(stdout);
+  int repeatingkey[];
+  printf("Enter a XOR value: ");                                                                                                                                                                
+  scanf("%d", repeatingkey);   
+  fflush(stdout);
+/* XOR  every byte of INPUTTED STRING VARIABLE */
+
+
+
+/* print the XORRED INPUTTED STRING VARIABLE */
+
+
+
+/*give user an option D/E(Decrypt existing/Encrypt a new)
+
+
+
+/*if Encrypt a new make a new variable and take a new input and perform line 3 to 7 again*/
+
+
+
+/*if Decrypt existing use the repeating key 11110011 to decrypt*/
+
+
+
+/*print out DECRYPTED INPUTTED VARIABLE*/
+return 0;
+}
