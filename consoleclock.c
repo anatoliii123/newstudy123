24dc20d083393cf7cdeda38cf464b0bde12a9b31
anatoliii123 <64612850+anatoliii123@users.noreply.github.com>
2022-06-17 10:36:25 -0500

    fixed indentetion

--- consoleclock.c
+++ consoleclock.c
@@ -1,3 +1,7 @@
+/*AUTHOR ANATOLII123*/
+/* ---->Use on unix compiler for best results<-----*/
+/* EX: https://www.onlinegdb.com/online_c_compiler */
+
 #include <stdio.h>
 #include <unistd.h>
 unsigned short int minute;
@@ -12,22 +16,29 @@ int main()
 printf("Enter seconds: ");
 scanf("%hu", &second);
 fflush(stdout);
+
 printf("Enter minute: ");
 scanf("%hu", &minute);
 fflush(stdout);
+
 printf("Enter hour: ");
 scanf("%hu", &hour);
 }
+
 while(second!=60)
 { 
 printf("\rSeconds %d", second);
 printf(" ");
+
 printf("Minute %d", minute);
 printf(" ");
+
 printf("Hour %d", hour);
 fflush(stdout);
+
 second++;
 sleep(1);
+
 }
 if (second == 60)
 {
