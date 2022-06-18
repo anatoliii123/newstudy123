cf41c274d81ff2a1778803a9514b5d37afd8ec53
anatoliii123 <64612850+anatoliii123@users.noreply.github.com>
2022-06-18 13:14:04 -0500

    improved it a bit, still invalid

--- encryptdecryptascii.c
+++ encryptdecryptascii.c
@@ -1,21 +1,11 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-void generate_a_key(char a)
-{
-    
-    char letters[26] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
-    srand(time(0));
-    int number1 = rand();
-    char letter1 = letters[number1];
-    a = letter1;
-
-}
 
 int main()
 {
-    char check_for_privatekey;
-    char private_key;
+    char private_key[20] = {"key"};
+    char check_for_privatekey[20];
     char text_to_encrypt[20];
     printf("Enter text to encrypt: ");
     if (fgets(text_to_encrypt, 20, stdin) == NULL) {
@@ -23,11 +13,23 @@ int main()
         exit(1);
     }
     // START PROCCESS
-    printf("Encrypting string...%s\n", text_to_encrypt);
-    generate_a_key(private_key);
-    printf("Your private key is:%c", private_key);
+    printf("Encrypting string->%s\n", text_to_encrypt);
+    puts("This is where private_key is assigned");
+    printf("Your private key is:%s\n", private_key);
     // "Encrypted with private key"
-    printf("")
+    printf("Enter private key: ");
+     if (fgets(check_for_privatekey,20,stdin) == NULL) {
+        printf("Input error.\n");
+        exit(1);
+    }
+    if (strcmp(private_key, check_for_privatekey) == 0)
+    {
+        printf("String decrypted\n %c", private_key);
+    }
+    else
+    {
+        printf("Invalid key");
+    }
     
     
     return 0;
