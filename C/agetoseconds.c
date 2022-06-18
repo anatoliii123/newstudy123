691e7bb38be4dea92237e41fd8edc2f789f51b8c
anatoliii123 <64612850+anatoliii123@users.noreply.github.com>
2022-06-18 11:11:02 -0500

    fixed indentation

---
+++ C/agetoseconds.c
@@ -0,0 +1,37 @@
+/*AUTHOR ANATOLII123
+  6/18/2022 */
+#include <stdio.h>
+#include <unistd.h>
+int main()
+{
+    
+while(1)
+// while loop for custom input
+{
+    
+// declare input variable and take input
+int age;
+printf("Whats your age?: ");
+if(1!=(scanf("%d", &age)))
+{
+    return 1;
+}
+
+/*IMPERATIVE STYLE MONTHS,DAYS,HOURS,MINUTES,SECONDS*/
+int months = age*12;
+printf("months = %d\n", months);
+
+int days = months*30;
+printf("days = %d\n", days);
+
+int hours = days*24;
+printf("hours = %d\n", hours);
+
+int minutes = hours*60;
+printf("minutes = %d\n", minutes);
+
+int seconds = minutes*60;
+printf("seconds = %d\n", seconds);
+}
+
+}
