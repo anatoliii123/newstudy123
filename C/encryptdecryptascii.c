f1371c83c1afac8cee2d2aca35474ff57dc3b65b
anatoliii123 <64612850+anatoliii123@users.noreply.github.com>
2022-06-20 15:33:55 -0500

    added a better key generator

--- C/encryptdecryptascii.c
+++ C/encryptdecryptascii.c
@@ -2,11 +2,38 @@
 #include <string.h>
 #include <stdlib.h>
 
+char *randstring(size_t length) {
+
+    static char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789,.-#'?!";        
+    char *randomString = NULL;
+
+    if (length) {
+        randomString = malloc(sizeof(char) * (length +1));
+
+        if (randomString) {            
+            for (int n = 0;n < length;n++) {            
+                int key = rand() % (int)(sizeof(charset) -1);
+                randomString[n] = charset[key];
+            }
+
+            randomString[length] = '\0';
+        }
+    }
+
+    return randomString;
+}
+
 int main()
 {
-    char private_key[20] = {"key"};
+    int tempint = 20;
+    char *newchar = randstring(tempint);
+    char private_key[20];
+    strcpy(newchar, private_key);
     char check_for_privatekey[20];
     char text_to_encrypt[20];
+
+
+    
     printf("Enter text to encrypt: ");
     if (fgets(text_to_encrypt, 20, stdin) == NULL) {
         printf("Input error.\n");
@@ -25,7 +52,7 @@ int main()
         printf("Input error.\n");
         exit(1);
     }
-    if (strncmp(check_for_privatekey, private_key, 3) == 0)
+    if (strncmp(check_for_privatekey, private_key, 10) == 0)
     {
         printf("String decrypted\n ##%s", text_to_encrypt);
     }
