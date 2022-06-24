f2af98a2dc8c0fa3ea1959934461c2caa3cfe208
anatoliii123 <64612850+anatoliii123@users.noreply.github.com>
2022-06-24 10:50:35 -0500

    fizbuzz 1.0

--- C/fizzbuzz.c
+++ C/fizzbuzz.c
@@ -1,17 +1,31 @@
 #include <stdio.h>
 int count;
-char fizz_buzz[20] = {"Fizz", "Buzz", "FizzBuzz"};
-
+char *fizz_buzz[20] = {"Fizz", "Buzz", "FizzBuzz"};
+int divisible_num1 = 3; int divisible_num2 = 5; int divisible_num3 = 15;
 int main()
 {
 for(count=0; count<100; count++)
 {
     int var_for_switch;
     printf("Press 1 to continue: ");
-    fgets(var_for_switch, 1, stdin);
+    scanf("%d", &var_for_switch);
     if(var_for_switch==1)
     {
-        printf("%d", count);
+        printf("%d\n", count);
+        if(!(count%divisible_num1))
+        {
+            printf("%s\n", fizz_buzz[1]);
+        }
+        
+        else if(!(count%divisible_num2))
+        {
+            printf("%s\n", fizz_buzz[2]);
+        }
+        
+        else if(!(count%divisible_num3))
+        {
+            printf("%s\n", fizz_buzz[3]);
+        }
     }
 
 }
