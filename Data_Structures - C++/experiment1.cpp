86aa746443200dda8e99f275fbf2b02cb1af635d
anatoliii123 <64612850+anatoliii123@users.noreply.github.com>
2022-07-21 15:12:36 -0500

    data-structure *_*

---
+++ Data_Structures - C++/experiment1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+struct employee {
+int age;
+string name;
+long int ssn;
+string dob;
+} manager, supervisor, regular_member;
+
+int main()
+{
+employee manager;
+manager.age = 21;
+
+}
+
+
